8e5f2c0201c3cc9c5782ea389f96381bbdc54e85
Mark Harviston <Mark.Harviston.-ND@disney.com>
2015-03-30 14:21:13 -0700

    cleanup build.sh
    mkdir build was in there because I thought output couldn't be captured
    earlier
    remove output.ulx so you can make sure it was created properly

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 docker build -t inform7-builder .
 touch story.ni
+rm -f output.ulx
 touch output.ulx
-mkdir -p build
 docker run -ti \
     -v "$PWD/story.ni":/root/project.inform/Source/story.ni:ro \
     -v "$PWD/output.ulx":/root/project.inform/Build/output.ulx \
