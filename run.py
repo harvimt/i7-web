72baa5197e6b73d5befdeeedfa1306f65e65d581
Mark Harviston <Mark.Harviston.-ND@disney.com>
2015-03-30 14:21:56 -0700

    Use temporary directory
    This way multiple builds can be going on at once (each build will get
    it's own temporary directory)

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3.4
 import subprocess
 from pathlib import Path
+import tempfile
+import shutil
 
 NI_PATH = Path('/usr/lib/x86_64-linux-gnu/gnome-inform7/ni')
 INFORM6_PATH = Path('/usr/lib/x86_64-linux-gnu/gnome-inform7/inform6')
 INFORM7_HOME = Path('/usr/share/gnome-inform7')
-project_path = Path('/root/project.inform')
+PROJECT_TEMPLATE_PATH = Path('/root/project.inform')
 
-print(subprocess.check_output(list(map(str, [NI_PATH, '-internal', INFORM7_HOME, '-format=ulx', '-project', project_path]))).decode('utf8'))
-print(subprocess.check_output(list(map(str, [INFORM6_PATH, '-wxE2kSDG', '$huge', project_path / 'Build/auto.inf', project_path / 'Build/output.ulx']))).decode('utf8'))
+with tempfile.TemporaryDirectory() as tempdir:
+    tempdir_path = Path(tempdir)
+    project_path = tempdir_path / 'project.inform'
+    shutil.copytree(str(PROJECT_TEMPLATE_PATH), str(project_path))
+
+    print(subprocess.check_output(list(map(str, [NI_PATH, '-internal', INFORM7_HOME, '-format=ulx', '-project', project_path]))).decode('utf8'))
+    print(subprocess.check_output(list(map(str, [INFORM6_PATH, '-wxE2kSDG', '$huge', project_path / 'Build/auto.inf', project_path / 'Build/output.ulx']))).decode('utf8'))
